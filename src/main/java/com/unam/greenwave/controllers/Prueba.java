af4839cddb1b3bae2334bcf9d2060651e0f4d5d2
Lucas Colman <lukasscolman@gmail.com>
2024-11-21 22:43:17 -0300

    Update Prueba.java

--- src/main/java/com/unam/greenwave/controllers/Prueba.java
+++ src/main/java/com/unam/greenwave/controllers/Prueba.java
@@ -1,4 +1,4 @@
-package com.unam.greenwave.controllers;
+package com.unam.greenwave.controllers; 
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
